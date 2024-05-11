de115dc08471f1238d28e73dc86ccae8dcb31f27
Satyam Subhash Sawant <satyamsawant54@gmail.com>
2024-05-12 02:32:06 +0530

    login page for username and password done

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Login = () => {
+  const [user, setUser] = useState({
+    username: "",
+    password: "",
+  });
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    console.log(user);
+    setUser({
+      username: "",
+      password: "",
+    });
+  };
+
   return (
     <div className="min-w-96 mx-auto">
       <div className="w-full p-12 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 border border-gray-100">
         <h1 className="text-3xl font-extrabold text-center text-white ">
           Login
         </h1>
-        <form action="" className="mt-5">
+        <form onSubmit={onSubmitHandler} action="" className="mt-5">
           <div>
             <label className="label p-2">
               <span className="text-base label-text text-black font-semibold">
@@ -16,6 +30,8 @@ const Login = () => {
               </span>
             </label>
             <input
+              value={user.username}
+              onChange={(e) => setUser({ ...user, username: e.target.value })}
               className="w-full input input-bordered h-10 bg-white"
               type="text"
               placeholder="username@gmail.com"
@@ -28,6 +44,8 @@ const Login = () => {
               </span>
             </label>
             <input
+              value={user.password}
+              onChange={(e) => setUser({ ...user, password: e.target.value })}
               className="w-full input input-bordered h-10 bg-white"
               type="password"
               placeholder="******"
@@ -42,7 +60,10 @@ const Login = () => {
           </p>
 
           <div>
-            <button className="btn btn-block btn-sm mt-3 border-slate-500 font-bold">
+            <button
+              type="submit"
+              className="btn btn-block btn-sm mt-3 border-slate-500 font-bold"
+            >
               Sign In
             </button>
           </div>
