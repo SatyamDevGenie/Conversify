cc2fbf044782b40bd512cad6401ede231181e06c
Satyam Subhash Sawant <satyamsawant54@gmail.com>
2024-05-28 11:49:05 +0530

    Implement store, userSlice.js in frontend using using redux-toolkit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,10 +4,15 @@ import { Toaster } from "react-hot-toast";
 import App from "./App";
 import "./index.css";
 
+import { Provider } from "react-redux";
+import store from "./redux/store";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
-    <Toaster />
+    <Provider store={store}>
+      <App />
+      <Toaster />
+    </Provider>
   </React.StrictMode>
 );
