c8bb33c88ed072d1630446ac69513c8e9fda066f
Your Name <your.email@example.com>
2024-07-27 20:03:35 +0530

    install react icons

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { BiSearchAlt2 } from "react-icons/bi";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <div>
+      <form action="">
+        <input
+          className="input input-bordered rounded-md"
+          type="text"
+          placeholder="Search..."
+        />
+        <button>
+          <BiSearchAlt2 className="w-6 h-6 outline-none" />
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Sidebar;
