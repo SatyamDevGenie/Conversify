9a120142960470358aed2be22527786770df735c
Your Name <your.email@example.com>
2024-07-27 20:14:47 +0530

    add some styling to sidebar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -3,17 +3,18 @@ import { BiSearchAlt2 } from "react-icons/bi";
 
 const Sidebar = () => {
   return (
-    <div>
-      <form action="">
+    <div className="border-r border-slate-500 p-4 flex flex-col">
+      <form action="" className="flex items-center gap-2">
         <input
           className="input input-bordered rounded-md"
           type="text"
           placeholder="Search..."
         />
-        <button>
+        <button type="submit" className="btn btn-circle bg-zinc-500 text-white">
           <BiSearchAlt2 className="w-6 h-6 outline-none" />
         </button>
       </form>
+      <div className="divider px-3"></div>
     </div>
   );
 };
