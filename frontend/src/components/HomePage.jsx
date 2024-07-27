446107edb86a4a7cd16b464a595dac10491b0373
Your Name <your.email@example.com>
2024-07-27 19:58:22 +0530

    add styling to homepage

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -4,7 +4,7 @@ import Sidebar from "./Sidebar";
 
 export const HomePage = () => {
   return (
-    <div>
+    <div className="flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
       <Sidebar />
       <MessageContainer />
     </div>
