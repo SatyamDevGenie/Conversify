fee3927d54d96f398c46987ef4c181f0128fb191
Your Name <your.email@example.com>
2024-07-28 11:53:43 +0530

    Message Container Component implemented

--- frontend/src/components/MessageContainer.jsx
+++ frontend/src/components/MessageContainer.jsx
@@ -2,9 +2,21 @@ import React from "react";
 
 const MessageContainer = () => {
   return (
-    <div>
-      <div>
-        <div></div>
+    <div className="md:min-w-[550px] flex flex-col">
+      <div className="flex gap-2 items-center bg-zinc-800 text-white font-semibold px-2 py-2 mb-2">
+        <div className="avatar online">
+          <div className="w-12 rounded-full">
+            <img
+              src="https://www.pngall.com/wp-content/uploads/5/Profile.png"
+              alt="user-profile"
+            />
+          </div>
+        </div>
+        <div className="flex flex-col flex-1">
+          <div className="flex justify-between gap-3 ">
+            <p>Satyam Sawant</p>
+          </div>
+        </div>
       </div>
     </div>
   );
