1961b3915af9bc5c4233bfcb6d490215edcc6db0
Your Name <your.email@example.com>
2024-07-30 18:34:34 +0530

    implement scroll bar and all stuffs

--- frontend/src/components/Messages.jsx
+++ frontend/src/components/Messages.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import Message from "./Message";
 
 const Messages = () => {
-  return <div className="px-4 flex-1 overflow-auto"></div>;
+  return (
+    <div className="px-4 flex-1 overflow-auto">
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+    </div>
+  );
 };
 
 export default Messages;
