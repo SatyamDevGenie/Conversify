734a8afeb49b3817d7747d9dbfd81b52f0f68729
Your Name <your.email@example.com>
2024-07-31 16:30:59 +0530

    implemented otherUsers in frontend using redux

--- frontend/src/components/OtherUser.jsx
+++ frontend/src/components/OtherUser.jsx
@@ -1,21 +1,20 @@
 import React from "react";
 
-const OtherUser = () => {
+const OtherUser = (props) => {
+  const user = props.user;
+
   return (
     <div>
       <div>
         <div className="flex gap-2 items-center text-white hover:text-zinc-800 font-bold hover:bg-zinc-200 rounded-lg p-2 cursor-pointer">
           <div className="avatar online">
             <div className="w-12 rounded-full">
-              <img
-                src="https://www.pngall.com/wp-content/uploads/5/Profile.png"
-                alt="user-profile"
-              />
+              <img src={user?.profilePhoto} alt="user-profile" />
             </div>
           </div>
           <div className="flex flex-col flex-1">
             <div className="flex justify-between gap-3">
-              <p>Satyam Sawant</p>
+              <p>{user?.fullName}</p>
             </div>
           </div>
         </div>
--- frontend/src/components/OtherUsers.jsx
+++ frontend/src/components/OtherUsers.jsx
@@ -1,20 +1,19 @@
+import { useSelector } from "react-redux";
 import useGetOtherUsers from "../hooks/useGetOtherUsers";
 import OtherUser from "./OtherUser";
 
 const OtherUsers = () => {
+  // my custom hooks
   useGetOtherUsers();
+  const { otherUsers } = useSelector((store) => store.user);
+
+  if (!otherUsers) return; // early return in react
 
   return (
     <div className="overflow-auto ">
-      <OtherUser />
-      <OtherUser />
-      <OtherUser />
-      <OtherUser />
-      <OtherUser />
-      <OtherUser />
-      <OtherUser />
-      <OtherUser />
-      <OtherUser />
+      {otherUsers?.map((user) => {
+        return <OtherUser key={user._id} user={user} />;
+      })}
     </div>
   );
 };
