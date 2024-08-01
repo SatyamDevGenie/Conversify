f2ad5bb4fb2af21c2f8bfb652d2b3d942d9a586c
Your Name <your.email@example.com>
2024-08-01 18:14:13 +0530

    imported in store.js file

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import messageReducer from "./messageSlice.js";
 import userReducer from "./userSlice.js";
 
 const store = configureStore({
   reducer: {
     user: userReducer,
+    message: messageReducer,
   },
 });
 
