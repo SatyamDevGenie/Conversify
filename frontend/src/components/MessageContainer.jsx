9cf9a81c924cc45851a9fdd16c5fc03752a0487c
Your Name <your.email@example.com>
2024-08-01 22:21:13 +0530

    dispatch selectedUser name in frontend

--- frontend/src/components/MessageContainer.jsx
+++ frontend/src/components/MessageContainer.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import Messages from "./Messages";
 import SendInput from "./SendInput";
 
 const MessageContainer = () => {
+  const { selectedUser } = useSelector((store) => store.user);
+
   return (
     <div className="md:min-w-[550px] flex flex-col">
       <div className="flex gap-2 items-center bg-zinc-800 text-white font-semibold px-2 py-2 mb-2">
@@ -16,7 +19,7 @@ const MessageContainer = () => {
         </div>
         <div className="flex flex-col flex-1">
           <div className="flex justify-between gap-3 ">
-            <p>Satyam Sawant</p>
+            <p>{selectedUser?.fullName}</p>
           </div>
         </div>
       </div>
