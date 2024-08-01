d0c5ba9550780f5fb6980370b1ec0557ef3a524c
Your Name <your.email@example.com>
2024-08-01 17:12:04 +0530

    SelectedUser data stored in redux devtools

--- frontend/src/components/OtherUser.jsx
+++ frontend/src/components/OtherUser.jsx
@@ -1,25 +1,35 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { setSelectedUser } from "../redux/userSlice";
 
-const OtherUser = (props) => {
-  const user = props.user;
+const OtherUser = ({ user }) => {
+  // const user = props.user;
+
+  const dispatch = useDispatch(); // to dispatchb action from redux store....
+
+  const selectedUserHandler = (user) => {
+    // console.log(user);
+    dispatch(setSelectedUser(user));
+  };
 
   return (
     <>
-      <div>
-        <div className="flex gap-2 items-center text-white hover:text-zinc-800 font-bold hover:bg-zinc-200 rounded-lg p-2 cursor-pointer">
-          <div className="avatar online">
-            <div className="w-12 rounded-full">
-              <img src={user?.profilePhoto} alt="user-profile" />
-            </div>
-          </div>
-          <div className="flex flex-col flex-1">
-            <div className="flex justify-between gap-3">
-              <p>{user?.fullName}</p>
-            </div>
+      <div
+        onClick={() => selectedUserHandler(user)}
+        className="flex gap-2 items-center text-white hover:text-zinc-800 font-bold hover:bg-zinc-200 rounded-lg p-2 cursor-pointer"
+      >
+        <div className="avatar online">
+          <div className="w-12 rounded-full">
+            <img src={user?.profilePhoto} alt="user-profile" />
           </div>
         </div>
-        <div className="divider my-0 py-0"></div>
+        <div className="flex flex-col flex-1">
+          <div className="flex justify-between gap-3">
+            <p>{user?.fullName}</p>
+          </div>
+        </div>
       </div>
+      <div className="divider my-0 py-0"></div>
     </>
   );
 };
