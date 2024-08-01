87237381cfcfc1b61adaec332410202bb1b4669c
Your Name <your.email@example.com>
2024-08-01 22:05:51 +0530

    define use

--- frontend/src/components/Messages.jsx
+++ frontend/src/components/Messages.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import useGetMessages from "../hooks/useGetMessages";
 import Message from "./Message";
 
 const Messages = () => {
   useGetMessages();
+
+  const { messages } = useSelector((store) => store.message);
+  if (!messages) return;
 
   return (
     <div className="px-4 flex-1 overflow-auto">
