875e44d20438fe76a88347e136e6885a50aa09c1
Your Name <your.email@example.com>
2024-08-01 22:15:23 +0530

    pass message as a prop in Message

--- frontend/src/components/Message.jsx
+++ frontend/src/components/Message.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Message = () => {
+const Message = ({ message }) => {
   return (
     <div className="chat chat-end">
       <div className="chat-image avatar">
@@ -14,7 +14,7 @@ const Message = () => {
       <div className="chat-header">
         <time className="text-xs opacity-50 text-white">12:45</time>
       </div>
-      <div className="chat-bubble">You were the Chosen One!</div>
+      <div className="chat-bubble">{message?.message}</div>
     </div>
   );
 };
