f4517cdfee680f85dad8a33e4e5252e15d55efdc
Your Name <your.email@example.com>
2024-08-01 22:12:04 +0530

    Messages component modify

--- frontend/src/components/Messages.jsx
+++ frontend/src/components/Messages.jsx
@@ -11,18 +11,10 @@ const Messages = () => {
 
   return (
     <div className="px-4 flex-1 overflow-auto">
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
+      {messages &&
+        messages.map((message) => {
+          return <Message key={message._id} message={message} />;
+        })}
     </div>
   );
 };
