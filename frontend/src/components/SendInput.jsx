cfa1483e0fce0fd40fb595798b5331b03e23778c
Your Name <your.email@example.com>
2024-08-02 10:31:07 +0530

    basic setup done in SendInput component

--- frontend/src/components/SendInput.jsx
+++ frontend/src/components/SendInput.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { IoSend } from "react-icons/io5";
 
 const SendInput = () => {
+  const [message, setMessage] = useState(""); //useState........
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    alert(message);
+  };
+
   return (
-    <form className="px-4 my-3">
+    <form onSubmit={onSubmitHandler} className="px-4 my-3">
       <div className="w-full relative">
         <input
+          value={message}
+          onChange={(e) => setMessage(e.target.value)} // onChange Event..........
           type="text"
           placeholder="Send a message..."
           className="border text-sm rounded-lg block w-full p-3 border-zinc-500 bg-gray-600 text-white"
