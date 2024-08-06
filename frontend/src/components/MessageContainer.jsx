89361dc4e67aaba9c138837c5a7636c3813c31ac
Your Name <your.email@example.com>
2024-08-06 09:34:33 +0530

    basic conditional rendering in MessageContainer

--- frontend/src/components/MessageContainer.jsx
+++ frontend/src/components/MessageContainer.jsx
@@ -7,22 +7,28 @@ const MessageContainer = () => {
   const { selectedUser } = useSelector((store) => store.user);
 
   return (
-    <div className="md:min-w-[550px] flex flex-col">
-      <div className="flex gap-2 items-center bg-zinc-800 text-white font-semibold px-2 py-2 mb-2">
-        <div className="avatar online">
-          <div className="w-12 rounded-full">
-            <img src={selectedUser?.profilePhoto} alt="user-profile" />
+    <>
+      {selectedUser !== null ? (
+        <div className="md:min-w-[550px] flex flex-col">
+          <div className="flex gap-2 items-center bg-zinc-800 text-white font-semibold px-2 py-2 mb-2">
+            <div className="avatar online">
+              <div className="w-12 rounded-full">
+                <img src={selectedUser?.profilePhoto} alt="user-profile" />
+              </div>
+            </div>
+            <div className="flex flex-col flex-1">
+              <div className="flex justify-between gap-3 ">
+                <p>{selectedUser?.fullName}</p>
+              </div>
+            </div>
           </div>
+          <Messages />
+          <SendInput />
         </div>
-        <div className="flex flex-col flex-1">
-          <div className="flex justify-between gap-3 ">
-            <p>{selectedUser?.fullName}</p>
-          </div>
-        </div>
-      </div>
-      <Messages />
-      <SendInput />
-    </div>
+      ) : (
+        <h1>Let's start the conversation</h1>
+      )}
+    </>
   );
 };
 
