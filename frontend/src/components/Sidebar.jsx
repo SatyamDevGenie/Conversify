4a18a786bf1f3ebb720f0e9aaa72d70bb5d3ce19
Your Name <your.email@example.com>
2024-08-07 15:12:13 +0530

    again Sidebar updated

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,11 +1,18 @@
 import axios from "axios";
-import React from "react";
+import React, { useState } from "react";
 import toast from "react-hot-toast";
 import { BiSearchAlt2 } from "react-icons/bi";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { setOtherUsers } from "../redux/userSlice";
 import OtherUsers from "./OtherUsers";
 
 const Sidebar = () => {
+  const [search, setSearch] = useState("");
+
+  const { otherUsers } = useSelector((store) => store.user);
+
+  const dispatch = useDispatch();
   const navigate = useNavigate(); // to navigate......
 
   const logoutHandler = async () => {
@@ -18,10 +25,28 @@ const Sidebar = () => {
     }
   };
 
+  const searchSubmitHandler = (e) => {
+    e.preventDefault();
+    const conversationUser = otherUsers.find((user) =>
+      user.fullName.toLowerCase().includes(search.toLowerCase())
+    );
+    if (conversationUser) {
+      dispatch(setOtherUsers([conversationUser]));
+    } else {
+      toast.error("User not found!");
+    }
+  };
+
   return (
     <div className="border-r border-slate-500 p-4 flex flex-col">
-      <form action="" className="flex items-center gap-2">
+      <form
+        onSubmit={searchSubmitHandler}
+        action=""
+        className="flex items-center gap-2"
+      >
         <input
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
           className="input input-bordered rounded-md"
           type="text"
           placeholder="Search..."
