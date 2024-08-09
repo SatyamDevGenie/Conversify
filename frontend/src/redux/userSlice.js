0fa3b7257dee077af0144b753ef5d290b6f25b98
Your Name <your.email@example.com>
2024-08-10 05:28:21 +0530

    userSlice updated

--- frontend/src/redux/userSlice.js
+++ frontend/src/redux/userSlice.js
@@ -23,31 +23,3 @@ const userSlice = createSlice({
 export const { setAuthUser, setOtherUsers, setSelectedUser } =
   userSlice.actions;
 export default userSlice.reducer;
-
-// below code for just for reference from chatGPT......
-
-// import { createSlice } from "@reduxjs/toolkit";
-
-// const userSlice = createSlice({
-//   name: "user",
-//   initialState: {
-//     authUser: null,
-//     otherUsers: [],
-//     selectedUser: null,
-//   },
-//   reducers: {
-//     setAuthUser: (state, action) => {
-//       state.authUser = action.payload;
-//     },
-//     setOtherUsers: (state, action) => {
-//       state.otherUsers = action.payload;
-//     },
-//     setSelectedUser: (state, action) => {
-//       state.selectedUser = action.payload;
-//     },
-//   },
-// });
-
-// export const { setAuthUser, setOtherUsers, setSelectedUser } =
-//   userSlice.actions;
-// export default userSlice.reducer;
