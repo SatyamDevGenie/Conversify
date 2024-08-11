ccbfa5094b3b62713d3dc78dc88ba2e22e8333a0
Your Name <your.email@example.com>
2024-08-11 17:42:32 +0530

    indexjs modify

--- backend/index.js
+++ backend/index.js
@@ -1,38 +1,3 @@
-// import cookieParser from "cookie-parser";
-// import cors from "cors";
-// import dotenv from "dotenv";
-// import express from "express";
-// import connectDB from "./config/database.js";
-// import messageRoute from "./routes/mesageRoute.js";
-// import userRoute from "./routes/userRoute.js";
-
-// dotenv.config({});
-
-// const app = express();
-
-// const PORT = process.env.PORT || 5000;
-
-// // middleware
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-// app.use(cookieParser());
-// const corsOption = {
-//   origin: "http://localhost:3000",
-//   credentials: true,
-// };
-// app.use(cors(corsOption));
-
-// // routes
-// app.use("/api/v1/user", userRoute);
-// app.use("/api/v1/message", messageRoute);
-
-// app.listen(PORT, () => {
-//   connectDB();
-//   console.log(`Server running on ${PORT}`);
-// });
-
-// ------------------------------------------------------------------------------------------------------>
-
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
