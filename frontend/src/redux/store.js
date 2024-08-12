9f381323b632a8a06bedd9e4604bc65c1b8f7b72
Your Name <your.email@example.com>
2024-08-13 04:12:16 +0530

    created socketSlice & setup done in store.js

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import messageReducer from "./messageSlice.js";
+import socketReducer from "./socketSlice.js";
 import userReducer from "./userSlice.js";
 
 const store = configureStore({
   reducer: {
     user: userReducer,
     message: messageReducer,
+    socket: socketReducer,
   },
 });
 
