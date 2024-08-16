0f59e85e86c8c008f6fec5e70dc67c5b9670e3dd
Your Name <your.email@example.com>
2024-08-16 14:45:09 +0530

    user connected using socket IO

--- backend/index.js
+++ backend/index.js
@@ -5,23 +5,9 @@ import express from "express";
 import connectDB from "./config/database.js";
 import messageRoute from "./routes/mesageRoute.js";
 import userRoute from "./routes/userRoute.js";
+import { app, server } from "./socket/socket.js";
 
 dotenv.config({});
-
-const app = express();
-// const server = http.createServer(app); // Create an HTTP server
-// const io = new Server(server, {
-//   cors: {
-//     origin: "http://localhost:3000",
-//     credentials: true,
-//   },
-// });
-
-// export const getReceiverSocketId = (receiverId) => {
-//   return userSocketMap[receiverId];
-// };
-
-// const userSocketMap = {}; // like this => {userId->socketId}
 
 const PORT = process.env.PORT || 5000;
 
@@ -39,26 +25,8 @@ app.use(cors(corsOption));
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/message", messageRoute);
 
-// Socket.IO connection event
-// io.on("connection", (socket) => {
-//   console.log("Socket connected:", socket.id);
-
-//   const userId = socket.handshake.query.userId;
-//   if (userId !== undefined) {
-//     userSocketMap[userId] = socket.id;
-//   }
-
-//   io.emit("getOnlineUsers", Object.keys(userSocketMap));
-
-//   socket.on("disconnect", () => {
-//     console.log("Socket disconnected:", socket.id);
-//     delete userSocketMap[userId];
-//     io.emit("getOnlineUsers", Object.keys(userSocketMap));
-//   });
-// });
-
 // Start server and database connection
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   connectDB();
   console.log(`Server running on ${PORT}`);
 });
